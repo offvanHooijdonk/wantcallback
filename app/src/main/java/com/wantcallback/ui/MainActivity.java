4e70bbdf0533ccd0f13ff5c6094a9ad22a54ce53
offvanhooidonk <offspring25@gmail.com>
2015-07-20 19:28:46 +0300

    Added library for FAB

--- app/src/main/java/com/wantcallback/ui/MainActivity.java
+++ app/src/main/java/com/wantcallback/ui/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends Activity implements AppEnableActionProvider.To
         } else {
             displayMainLayout(false);
         }
-
+// implement FAB
         btnAddAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
